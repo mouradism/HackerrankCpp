8f7fe5618124fa566772c706107e4f95e89e7e20
mouradism <mourad.smail@gmail.com>
2020-10-09 18:03:57 +0100

    variable sized arrays cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> a;
+    a.push_back(1);
+    //a[i].push_back(temp)   ;
+    cout << a[0];
+    return 0;
+}
